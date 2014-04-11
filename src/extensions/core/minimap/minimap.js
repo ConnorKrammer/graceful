d7f53d1772a5f22e12c34afe108012bbdda01b41
Connor Krammer <connork99@gmail.com>
2014-04-10 23:42:20 -0600

    Allow pane minimaps to be toggled

--- src/extensions/core/minimap/minimap.js
+++ src/extensions/core/minimap/minimap.js
@@ -19,11 +19,6 @@
    * Creates a minimap that displays the editor content and can be used
    * as a draggable scrollbar.
    *
-   * @todo Allow the calling the mini command a second time to remove the
-   *       minimap from the current pane.
-   * @todo Clean up event listeners when removing a minimap, or when closing
-   *       the minimap's pane.
-   *
    * @constructor
    * @param {InputPane} pane - The pane to add the minimap to.
    * @return {MiniMap} The created minimap.
@@ -60,55 +55,28 @@
     // Get the border width. Assume all sides are the same thickness.
     borderWidth = parseInt(getComputedStyle(overlay).borderLeftWidth, 10);
 
-    // Update the display on resize.
-    pane.on('resize', function() { _this.updateDisplay(); });
-
-    // Update the content when text changes.
-    // Not throttled, since the change event already is.
-    pane.on('change.content', function() { _this.update(true, true); });
-
-    // Reset everything if the pane switches its buffer.
-    pane.on('change.buffer', function() { _this.update(true, true, true); });
-
-    // Update content when scrolling.
-    pane.cm.on('scroll', function() { _this.updateContent(); });
+    // Keep everything updated.
+    this.eventIDs = [
+      pane.on('resize',               function() { _this.updateDisplay(); }),
+      pane.on('change.content',       function() { _this.update(true, true, false); }),
+      pane.on('change.buffer',        function() { _this.update(true, true, true); }),
+      pane.on('scroll',               _.throttle(function() { _this.updateContent(); }, 30)),
+      pane.on('minimap.remove.start', function() { _this.deconstruct(); })
+    ];
 
     // Drag information.
     this.dragInfo = {
-      dragHandler: function(event) {
-        _this.dragHandler(event);
-        _this.updateContent();
-      },
+      pressHandler:   function(event) { _this.pressHandler(event); },
+      releaseHandler: function(event) { _this.releaseHandler(event); },
+      dragHandler:    function(event) { _this.dragHandler(event); },
       isDrag: false,
       lastY: 0,
       deltaY: 0
     };
 
-    // Start the drag on mousedown.
-    overlay.addEventListener('mousedown', function(event) {
-      // Prevent the drag from selecting text.
-      event.preventDefault();
-
-      // Initialize drag variables.
-      _this.dragInfo.isDrag = true;
-      _this.dragInfo.lastY = event.screenY;
-      _this.dragInfo.deltaY = 0;
-
-      // Add the drag handler.
-      document.addEventListener('mousemove', _this.dragInfo.dragHandler);
-    });
-
-    // Cancel the drag on mouseup.
-    document.addEventListener('mouseup', function(event) {
-      // Reset not needed if a drag wasn't triggered.
-      if (!_this.dragInfo.isDrag) return;
-
-      // Reset drag and cursor state.
-      _this.dragInfo.isDrag = false;
-
-      // Remove the drag handler.
-      document.removeEventListener('mousemove', _this.dragInfo.dragHandler);
-    });
+    // Start the drag on mousedown, cancel on mouseup.
+    overlay.addEventListener('mousedown', this.dragInfo.pressHandler);
+    document.addEventListener('mouseup', this.dragInfo.releaseHandler);
 
     // Store references.
     this.pane    = pane;
@@ -123,6 +91,9 @@
     // For tracking the scroll position.
     this.scrollPosition = {};
 
+    // For tracking visiblity state.
+    this.isVisible = true;
+
     // Update everything.
     this.update(true, true, true);
 
@@ -132,7 +103,7 @@
     minimap.offsetHeight;
 
     // Fade in if the minimap is enabled.
-    if (pane.wrapper.classList.contains('minimap-on')) {
+    if (this.isVisible) {
       toggleTransition(minimap, 'opacity', 'opacity 0.5s ease-in', '');
       minimap.style.opacity = 1;
     } else {
@@ -144,6 +115,36 @@
   }
 
   /**
+   * Removes the minimap and its event listeners.
+   *
+   * The removal happens in two stages: First, 'minimap.remove.start'
+   * fades out the minimap and begins removal of all event listeners.
+   * Then 'minimap.remove.end' removes the minimap and all evidence 
+   * of its existence from the DOM entirely.
+   */
+  MiniMap.prototype.deconstruct = function() {
+    var _this = this;
+
+    // Fade out if visible.
+    if (this.isVisible) this.updateDisplay(true);
+
+    // Remove DOM event listeners.
+    this.overlay.removeEventListener('mousedown', this.dragInfo.pressHandler);
+    document.removeEventListener('mouseup', this.dragInfo.releaseHandler);
+
+    // Remove pane event listeners.
+    this.pane.off(this.eventIDs);
+
+    // Remove the minimap from the DOM and clean
+    // up remaining classes on the pane wrapper.
+    this.pane.once('minimap.remove.end', function() {
+      _this.pane.wrapper.removeChild(_this.minimap);
+      _this.pane.wrapper.classList.remove('minimap-on');
+      _this.pane.wrapper.classList.remove('minimap-off');
+    });
+  };
+
+  /**
    * Convenience method to batch update types.
    *
    * @param {Boolean} [content=false]      - Whether or not to call updateContent().
@@ -151,9 +152,9 @@
    * @param {Boolean} [display=false]      - Whether or not to call updateDisplay().
    */
   MiniMap.prototype.update = function(content, innerContent, display) {
+    if (display)      this.updateDisplay();
     if (content)      this.updateContent();
     if (innerContent) this.updateInnerContent();
-    if (display)      this.updateDisplay();
   };
 
   /**
@@ -161,6 +162,40 @@
    */
   MiniMap.prototype.updateScroll = function() {
     this.scrollToPosition(this.cm.getScrollInfo().top);
+  };
+
+  /**
+   * Starts listening for mouse movement (starts drag detection).
+   *
+   * @param {Event} event - A mousedown event.
+   */
+  MiniMap.prototype.pressHandler = function(event) {
+    // Prevent the drag from selecting text.
+    event.preventDefault();
+
+    // Initialize drag variables.
+    this.dragInfo.isDrag = true;
+    this.dragInfo.lastY = event.screenY;
+    this.dragInfo.deltaY = 0;
+
+    // Add the drag handler.
+    document.addEventListener('mousemove', this.dragInfo.dragHandler);
+  };
+
+  /**
+   * Stops listening for mouse movement (ends drag detection).
+   *
+   * @param {Event} event - A mouseup event.
+   */
+  MiniMap.prototype.releaseHandler = function(event) {
+    // Reset not needed if a drag wasn't triggered.
+    if (!this.dragInfo.isDrag) return;
+
+    // Reset drag and cursor state.
+    this.dragInfo.isDrag = false;
+
+    // Remove the drag handler.
+    document.removeEventListener('mousemove', this.dragInfo.dragHandler);
   };
 
   /**
@@ -202,6 +237,9 @@
 
     // Scroll to the correct location.
     this.scrollToPosition(scrollPosition);
+
+    // Update content.
+    this.updateContent();
   };
 
   /**
@@ -256,17 +294,32 @@
   /**
    * Updates the area of the minimap that contains the editor
    * viewport text (what is currently seen on screen).
+   *
+   * @todo Allow syntax highlighting of minimap content.
+   *       Unfortunately, CodeMirror can't be used to highlight the
+   *       scrollbar for performance reasons. Just switch the comments
+   *       on the CodeMirror.runMode() line and the preview.innerHTML()
+   *       line to see the difference (the difference in performance will
+   *       most likely depend on what mode is being used).
+   *
+   *       One idea would be to wait for the user to finish scrolling then
+   *       do a soft fade between plain text and highlighting.
    */
   MiniMap.prototype.updateContent = function() {
+    var range, text, lines, scroll, lineTop;
+
+    if (!this.isVisible) return;
+
     // Get the text and calculate its offset.
-    var range   = this.cm.getViewport();
-    var text    = this.cm.doc.getRange({ line: range.from, ch: 0 }, { line: range.to, ch: 0 });
-    var lines   = this.cm.getWrapperElement().getElementsByClassName('CodeMirror-code')[0];
-    var scroll  = this.cm.getScrollerElement();
-    var lineTop = lines.getBoundingClientRect().top - scroll.getBoundingClientRect().top;
+    range   = this.cm.getViewport();
+    text    = this.cm.doc.getRange({ line: range.from, ch: 0 }, { line: range.to, ch: 0 });
+    lines   = this.cm.getWrapperElement().getElementsByClassName('CodeMirror-code')[0];
+    scroll  = this.cm.getScrollerElement();
+    lineTop = lines.getBoundingClientRect().top - scroll.getBoundingClientRect().top;
 
     // Set and position the preview text.
-    CodeMirror.runMode(text, this.cm.getOption('mode'), this.preview);
+    // CodeMirror.runMode(text, this.cm.getOption('mode'), this.preview);
+    this.preview.innerHTML = text;
     this.preview.style.top = lineTop + 'px';
 
     // Update the scroll position.
@@ -279,9 +332,12 @@
    * doesn't utilize CodeMirror's mode capabilities.
    */
   MiniMap.prototype.updateInnerContent = function() {
-    var text = this.cm.doc.getValue();
+    var text;
+
+    if (!this.isVisible) return;
 
     // If the last line is empty, append a space so that it renders.
+    text  = this.cm.doc.getValue();
     text += (text[text.length - 1] === '\n') ? ' ' : '';
 
     this.inner.textContent = text;
@@ -292,8 +348,10 @@
    * That the method of determining the pane's width is to find the percent width of
    * the pane and calculate the pixel width based on that. This is to avoid calculation
    * errors from ocurring in cases where a transition is in progress.
+   *
+   * @param {Boolean} hide - Whether to force the minimap to hide.
    */
-  MiniMap.prototype.updateDisplay = function() {
+  MiniMap.prototype.updateDisplay = function(hide) {
     var wrapper         = this.pane.wrapper;
     var measureTarget   = wrapper.parentElement.classList.contains('vertical-splitter-pane')
       ? wrapper.parentElement
@@ -304,17 +362,24 @@
     var minimapWidth    = this.minimap.getBoundingClientRect().width;
     var editorWidth     = minimapWidth / this.scale;
     var padding         = 10;
+    var oldValue        = this.isVisible;
 
-    // If the mini-map fits, display it.
-    if (editorWidth + minimapWidth + padding < paneWidth) {
+    // If the mini-map fits, display it (unless hide === true).
+    if (!hide && editorWidth + minimapWidth + padding < paneWidth) {
       wrapper.classList.add('minimap-on');
       wrapper.classList.remove('minimap-off');
+
+      this.isVisible = true;
+
+      // Update visible elements.
+      if (this.isVisible !== oldValue) this.update(true, true, false);
+      this.updateScroll();
     } else {
       wrapper.classList.add('minimap-off');
       wrapper.classList.remove('minimap-on');
-    }
 
-    this.updateScroll();
+      this.isVisible = false;
+    }
   };
 
   /**
@@ -348,16 +413,46 @@
  * ======================================================= */
 
   /**
-   * When graceful is loaded, define the 'mini' command to
-   * create a minimap on the current pane.
-   * The command gives other commands priority.
+   * When graceful is loaded, define the 'mini'
+   * command to toggle a minimap on the current pane.
    */
   graceful.onLoad(function() {
     graceful.editor.defineCommand({
       name: 'mini',
-      forceLast: true,
       func: function(pane) {
-        var minimap = new MiniMap(pane);
+        var deferred = Q.defer();
+        var minimap, waitForTransition, element;
+
+        // Minimaps only work on input panes.
+        if (pane instanceof Editor.InputPane === false) return;
+
+        // Check for an existing minimap, and toggle.
+        if (pane.wrapper.getElementsByClassName('minimap').length === 0) {
+          minimap = new MiniMap(pane);
+          waitForTransition = minimap.isVisible;
+          pane.trigger('minimap.add');
+        } else {
+          waitForTransition = pane.wrapper.classList.contains('minimap-on');
+          pane.trigger('minimap.remove.start');
+          if (!waitForTransition) pane.trigger('minimap.remove.end');
+        }
+
+        // Wait for the transition to finish.
+        if (waitForTransition) {
+          element = pane.cm.getWrapperElement();
+
+          element.addEventListener('transitionend', function listen(event) {
+            if (event.propertyName === '-webkit-transform') {
+              deferred.resolve();
+              pane.trigger('minimap.remove.end');
+              element.removeEventListener('transitionend', listen);
+            }
+          });
+        } else {
+          deferred.resolve();
+        }
+
+        return deferred.promise;
       }
     });
   });
